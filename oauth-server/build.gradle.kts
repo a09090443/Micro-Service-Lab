042cf8d3831ca9c334e268e7908f0f84d90edb77
zipe <zipe.daden@gmail.com>
2020-02-26 22:53:21 +0800

    Modify Gradle config file
    1. Root Gradle adds Java plug
    2. Oauth-server adds thymeleaf module

--- oauth-server/build.gradle.kts
+++ oauth-server/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-devtools")
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
