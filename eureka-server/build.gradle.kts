113db52ca5a644679841efcb751791597cc3f47d
Gary Tsai <garytsai@catchplay.com>
2020-05-29 16:32:20 +0800

    Complete erueka server and add banners

---
+++ eureka-server/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    kotlin("jvm")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+}
