5288f42d8ef0631b10f0d10c7c59e6b07c4466db
zipe <zipe.daden@gmail.com>
2020-06-05 22:47:13 +0800

    Update Application.kt, Application.kt, and 3 more files...

--- oauth-server/src/main/kotlin/com/zipe/Application.kt
+++ oauth-server/src/main/kotlin/com/zipe/Application.kt
@@ -1,12 +1,12 @@
 package com.zipe
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
+import org.springframework.boot.runApplication
 
 @SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 class Application
 
 fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
+    runApplication<Application>(*args)
 }
