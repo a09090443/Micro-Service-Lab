2315f296e87f0add4d585be3b60debad091e4d93
Gary Tsai <garytsai@catchplay.com>
2020-06-15 16:48:11 +0800

    Update build.gradle.kts

--- product-api-server/build.gradle.kts
+++ product-api-server/build.gradle.kts
@@ -3,33 +3,5 @@ plugins {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-
-    implementation("org.springframework.boot:spring-boot-starter-tomcat")
-    implementation("org.springframework.boot:spring-boot-devtools")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-logging")
-
-    implementation("org.springframework.cloud:spring-cloud-starter-config")
-    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-//    implementation("org.springframework.cloud:spring-cloud-starter-oauth2")
-
-    implementation("com.zaxxer:HikariCP")
-    implementation("org.hibernate:hibernate-ehcache")
-    implementation("mysql:mysql-connector-java")
-    implementation("com.thedeanda:lorem:${property("loremVersion")}")
-    implementation("javax.validation:validation-api:${property("validationApiVersion")}")
-    implementation("com.googlecode.log4jdbc:log4jdbc:${property("log4jdbcVersion")}")
-
-    implementation("com.graphql-java:graphql-spring-boot-starter:${property("graphqlVersion")}")
-    implementation("com.graphql-java:graphiql-spring-boot-starter:${property("graphqlVersion")}")
-    implementation("com.graphql-java:graphql-java-tools:${property("graphqlJavaVersion")}")
-    implementation(project(":core"))
+    api(project(":core"))
 }
-
-//springBoot {
-//    mainClassName = "com.zipe.Application"
-//}
